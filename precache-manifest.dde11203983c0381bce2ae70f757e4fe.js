36e4cb551db6babb4052b314ade5c7be6fcefd8b
NLJZ <johnson.nathan.lee@gmail.com>
2020-09-26 12:43:45 +0200

    Updates

---
+++ precache-manifest.dde11203983c0381bce2ae70f757e4fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb3e5fbda5f6dd1533daf4450ed6b38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c72d1d1a35b43014a8e",
+    "url": "./static/css/main.33176136.chunk.css"
+  },
+  {
+    "revision": "f87a6197a57b5ca50a52",
+    "url": "./static/js/2.6f9b680b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6f9b680b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c72d1d1a35b43014a8e",
+    "url": "./static/js/main.459eaef1.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "e3b1903e9724b59437c0126cc42b9e59",
+    "url": "./static/media/differenthand.e3b1903e.jpg"
+  },
+  {
+    "revision": "5b887a9be214e67c8658b3a9170efd2a",
+    "url": "./static/media/handstyping7.5b887a9b.jpg"
+  },
+  {
+    "revision": "59c0bf0888faddac8d02a919220793c8",
+    "url": "./static/media/me2.59c0bf08.jpg"
+  }
+]);
