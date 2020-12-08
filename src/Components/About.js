b2c8b20dc7b76b69c3a59aedf79cbce710982676
NLJZ <johnson.nathan.lee@gmail.com>
2020-12-08 18:02:02 +0100

    shorten about section:

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,40 +23,6 @@ const About = () => {
           <img src={Me} alt="portrait of the programmer" />
           <figcaption>Here I am!</figcaption>
         </figure>
-        <p>
-          Since the popularization of the Internet in the mid-90s I have used
-          web technologies as a hobbyist. When a friend or family member needed
-          help with a website I was their man. I built simple blogs, band pages,
-          portfolios for photographers. Lately, with my eye on a career change,
-          I have been significantly leveling up my programming skills. First
-          with the{" "}
-          <a
-            href="https://careerfoundry.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CareerFoundry
-          </a>{" "}
-          bootcamp (Ruby on Rails stack) and over the last year at the{" "}
-          <a
-            href="https://digitalcareerinstitute.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Digital Career Institute
-          </a>{" "}
-          in Berlin (MERN stack: MongoDB, Express, React, Node).
-        </p>
-        <p>
-          As browsers and web tech have become more sophisticated, memory and
-          storage space has grown exponentially, and our daily lives - from
-          shopping and communication all the way to how we consume art - have
-          gone increasingly digital, I am more intrigued than ever about the
-          possibilites of the web. My programming knowledge is growing daily. I
-          am excited to learn more so that I can combine programming with my
-          creative instincts to actualize fun and engaging web projects and
-          features.
-        </p>
       </div>
     </section>
   );
